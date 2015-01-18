b2fe5f4df08e2a8ab1c52c9d1f2c1e42b100cf71
Nicholai de Guzman <Nicholai@cchinfatt.local>
2015-01-17 20:35:30 -0600

    added TaskCell cocoa touch

---
+++ Taskit/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  Taskit
+//
+//  Created by Nicholai de Guzman on 1/17/15.
+//  Copyright (c) 2015 Nicholai de Guzman. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
